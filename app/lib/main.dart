d2d48283487520dc5c81773440c8ff5f5b6e07c9
JelsB <boulangier.jels@gmail.com>
2023-05-28 12:47:17 +0200

    Add amplidy auth and api

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import 'amplifyconfiguration.dart';
 import 'models/ModelProvider.dart';
 
 Future<void> main() async {
@@ -10,7 +15,24 @@ Future<void> main() async {
 }
 
 Future<void> _configureAmplify() async {
-  // To be filled in
+  try {
+    // Create the API plugin.
+    //
+    // If `ModelProvider.instance` is not available, try running
+    // `amplify codegen models` from the root of your project.
+    final api = AmplifyAPI(modelProvider: ModelProvider.instance);
+
+    // Create the Auth plugin.
+    final auth = AmplifyAuthCognito();
+
+    // Add the plugins and configure Amplify for your app.
+    await Amplify.addPlugins([api, auth]);
+    await Amplify.configure(amplifyconfig);
+
+    safePrint('Successfully configured');
+  } on Exception catch (e) {
+    safePrint('Error configuring Amplify: $e');
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -28,9 +50,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: _router,
-      debugShowCheckedModeBanner: false,
+    return Authenticator(
+      child: MaterialApp.router(
+        routerConfig: _router,
+        debugShowCheckedModeBanner: false,
+        builder: Authenticator.builder(),
+      ),
     );
   }
 }
@@ -78,7 +103,7 @@ class _HomeScreenState extends State<HomeScreen> {
   double _calculateTotalBudget(List<BudgetEntry?> items) {
     var totalAmount = 0.0;
     for (final item in items) {
-      totalAmount += item?.amount ?? 0;
+      totalAmount = item?.amount ?? 0;
     }
     return totalAmount;
   }
