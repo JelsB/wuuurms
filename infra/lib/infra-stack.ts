0bfd33df0f86fbf976ee55f9fca3315d879f9140
JelsB <boulangier.jels@gmail.com>
2023-12-21 15:55:15 +0100

    add Prettier formatting + shared vscode config

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,10 +1,12 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib'
+import { Construct } from 'constructs'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+interface A {
+  f: String
+}
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     // The code that defines your stack goes here
 
