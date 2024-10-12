a51f635313849772090dcbf5e5223a42bf1e593b
JelsB <boulangier.jels@gmail.com>
2024-10-12 18:45:29 +0200

    capture request event and context

--- infra/v2/stacks/api/api/handlers/main.py
+++ infra/v2/stacks/api/api/handlers/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 from mangum import Mangum
-# from aws_lambda_powertools.utilities.typing.lambda_context import LambdaContext
+from starlette.requests import Request
+
+from aws_lambda_powertools.utilities.typing.lambda_context import LambdaContext
+from aws_lambda_powertools.utilities.data_classes import event_source, APIGatewayProxyEvent
 
 app = FastAPI()
 
 
 @app.get('/ping')
-def ping():
+def ping(request: Request):
+    # Note: event and context from AWS Lambda Powertools are only available inside the Starlette request.
+    # This is different from using the Powertools app event resolvers.
+    # This can most likely also be turned into the decorator form. Maybe we need our own one for this.
+    event = APIGatewayProxyEvent(request.scope['aws.event'])
+    context: LambdaContext = request.scope['aws.context']
+
+    print(f'{event.raw_event=}')
+    print(f'{context=}')
     return {'message': 'pong'}
 
 
