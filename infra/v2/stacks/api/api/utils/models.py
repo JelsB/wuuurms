c1622c179bd41673a4e92930de324992ef229cda
JelsB <boulangier.jels@gmail.com>
2024-11-24 19:05:08 +0100

    add a bit of docs to optional model decorator

--- infra/v2/stacks/api/api/utils/models.py
+++ infra/v2/stacks/api/api/utils/models.py
@@ -8,7 +8,29 @@ from pydantic.fields import FieldInfo
 BaseModelT = TypeVar('BaseModelT', bound=BaseModel)
 
 
+# It is not and will not be possible to create Optional Pydantic Models based on another model
+# because of the current type implementation of Python
+# see: https://github.com/pydantic/pydantic/issues/3120
 def optional(model: Type[BaseModelT]) -> Type[BaseModelT]:
+    """
+    Creates a new Pydantic Model with all fields optional
+
+    Args:
+        model: The Pydantic Model to make all fields optional from
+
+    Example:
+    ```python
+    from pydantic import BaseModel
+
+    class User(BaseModel):
+        first_name: str
+        last_name: str
+
+    @optional
+    class OptionalUser(User):
+        pass
+    """
+
     def make_field_optional(field: FieldInfo, default: Any = None) -> Tuple[Any, FieldInfo]:
         new = deepcopy(field)
         # if not isinstance(field.default, PydanticUndefinedType):
